82a4ec4925d304268785c2f49e8341ea4a3ffbfb
Test <lukichev227@mail.ru>
2019-03-07 19:12:13 +0300

    0.3
    0.3

---
+++ SmartHouse/Assets/Scripts/Switch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject light;
+    public GameObject ceilingLight;
+    bool onOff = false;
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerStay()
+    {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            try
+            {
+                gameObject.GetComponent<AudioSource>().Play();
+                if (onOff)
+                {
+                    light.GetComponent<Light>().enabled = false;
+                    ceilingLight.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.black);
+                }
+                else
+                {
+                    light.GetComponent<Light>().enabled = true;
+                    ceilingLight.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", new Color(255f, 255f, 255f));
+                }
+                onOff = !onOff;
+            }catch(Exception)
+            {
+            }
+        }
+    }
+}
